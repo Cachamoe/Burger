529bc2e02b75f2a1216ebf4c251c62e8dde2bb86
Caleb Chaney <calebc347@gmail.com>
2020-10-29 10:58:45 -0500

    Updated server.js and reinstalled node_modules

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+// Require Express
 let express = require("express");
 
-// Sets up Express 
+// Set up Express 
 let app = express();
 let PORT = process.env.PORT || 3000;
 let db = require("./db/db.json");
 
 
-// Sets up Express to handle data parsing
+// Set up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
