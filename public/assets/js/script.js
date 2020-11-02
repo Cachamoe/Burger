9846cc66c17aea8624a268520e74935a64081611
Caleb Chaney <calebc347@gmail.com>
2020-11-02 08:36:21 -0600

    Updated Javascript, CSS, and Handlebars

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,7 +3,6 @@ $(".addBurgerBtn").on("click", function(event) {
     var burger = {
         burger_name: $("#burgerName").val()
     }
-    alert("Click");
     $.ajax({
         url: "/api/burgers", 
         method: "POST",
