0f6a48d80a0067d15de279633a41d4ae089adb77
Caleb Chaney <calebc347@gmail.com>
2020-11-03 15:19:22 -0600

    Updated Javascript

--- config/orm.js
+++ config/orm.js
@@ -3,52 +3,42 @@ let connection = require("../config/connection.js");
 const { insertOne } = require("../models/burger.js");
 
 
-// Function for converting object into SQL syntax
-function objToSql(obj) {
-    let arr = [];
-
-    for (var key in obj) {
-        var value = obj[key];
-        if (Object.hasOwnProperty.call(obj, key))
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value = "'" + value + "'";
-            };
-        arr.push(key + "=" + value);
-    };
-    return arr.toString();
-}
-
 // create the methods that will execute the necessary MySQL commands in the controllers.
 // These are the methods you will need to use in order to retrieve and store data in your database.
 let orm = {
     selectAll: function (tableName, cbModel) {
         connection.query("SELECT * FROM ??", tableName, function (err, results) {
             cbModel(results);
-        })
+        });
     },
     insertOne: function (tableName, cols, vals, cbModel) {
         connection.query("INSERT INTO ?? (??,??) VALUES (?,?)", [tableName, cols[0], cols[1], vals[0], vals[1]], function (err, results) {
             cbModel(results);
-        })
+        });
     },
     updateOne: function (tableName, objColVals, condition, cbModel) {
-        let queryString = "UPDATE " + tableName;
-
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
-
-        console.log(queryString);
-        connection.query(queryString, function (err, results) {
-            if (err) {
-                throw err;
-            }
+        connection.query("UPDATE ?? SET ?? WHERE ?", [tableName, objColVals, condition], function (err, results) {
             cbModel(results);
         });
     },
 }
 
-
 // Export the orm 
 module.exports = orm;
+
+// updateOne: function (tableName, objColVals, condition, cbModel) {
+//     let queryString = "UPDATE " + tableName;
+
+//     queryString += " SET ";
+//     queryString += objToSql(objColVals);
+//     queryString += " WHERE ";
+//     queryString += condition;
+
+//     console.log(queryString);
+//     connection.query(queryString, function (err, results) {
+//         if (err) {
+//             throw err;
+//         }
+//         cbModel(results);
+//     });
+// },
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -23,28 +23,43 @@ router.post("/api/burgers", function (req, res) {
         "burger_name", "devoured"
     ], [
         req.body.burger_name, 0
-    ], function (result) {
-        res.json({ id: result.insertId })
+    ], function (results) {
+        res.json({ id: results.insertId })
     });
 });
 
-
 router.put("/api/burgers/:id", function (req, res) {
     let condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-
     burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function (result) {
-        if (result.changedRows == 0) {
+        devoured: req.body.devoured,
+    }, condition, function (results) {
+        if (results === 0) {
             return res.status(404).end();
         } else {
             res.status(200).end();
         }
-    });
-});
+    },
+    );
+})
 
 // Export routes 
 module.exports = router;
+
+// router.put("/api/burgers/:id", function (req, res) {
+//     let condition = "id = " + req.params.id;
+
+//     console.log("condition", condition);
+
+// burger.updateOne({
+//     devoured: req.body.devoured
+// }, condition, function (results) {
+//     if (results.changedRows == 0) {
+//         return res.status(404).end();
+//     } else {
+//         res.status(200).end();
+//     }
+// });
+
+
+
 
